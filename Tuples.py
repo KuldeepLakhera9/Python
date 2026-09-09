8c9362a2f53f967fbc52ed28c36b15109c98edb4
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-09-09 12:23:03 +0530

    Basic Tuples

---
+++ Tuples.py
@@ -0,0 +1,3 @@
+coordinates = ((47, -182),(12, 34),(-6, -28))
+print(coordinates[0][1])
+
--- Variables&Datatype.py
+++ Variables&Datatype.py
@@ -2,4 +2,4 @@ character_name = "Krishna"
 character_age = "21"
 
 print("There was a man named " + character_name + ",")
-print("He was " + character_age + " years old")
+print("He was " + character_age + " years old")
