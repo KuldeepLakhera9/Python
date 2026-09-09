0e936e2c5903f90eebb60ef5d2c25934c4ee2537
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-09-09 11:32:52 +0530

    Learn Numbers

---
+++ Numbers.py
@@ -0,0 +1,22 @@
+from math import *
+
+print(3.14159)
+print(-50)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+print(10 % 3)
+
+my_num = 5
+
+print(my_num)
+print(str(my_num) + " Is my Favorite Number")
+
+my_num = -5.1
+print(my_num)
+print(abs(my_num))   # Absolute Value
+print(round(my_num))  # Round to nearest integer
+print(pow(my_num, 2)) # Power
+print(2 ** 3)       # Power
+print(10 // 3)      # Floor Division
+print(ceil(3.7))    # Ceiling
+print(sqrt(16)) # Square Root
