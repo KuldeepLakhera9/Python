730cede2b92131a9afe33547c3895c5dc4f28a9a
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-09-09 11:20:46 +0530

    Learn Strings

---
+++ Strings.py
@@ -0,0 +1,13 @@
+print("Unbuiltweb")
+print("Unbuilt\nweb")
+print("Unbuilt\\web")
+print("Unbuilt\"web")
+
+phrase = "Kuldeep Lakhera"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.islower())
+print(len(phrase))
+print(phrase[4])
+print(phrase[0:4])
